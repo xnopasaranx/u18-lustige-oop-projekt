befa0ebe2dad0ade9ccfb58011e53767dc22c90e
xnopasaranx <t85austria.nerd@gmail.com>
2026-02-27 09:30:59 +0100

    merged our code in one file

--- adam.py
+++ adam.py
@@ -49,3 +49,18 @@ class Thermostat(Device):
 
     def turn_on(self):
         self.__is_on = True
+
+
+class Management(object):
+    __owner = None
+    __all_devices = None
+
+    def __init__(self, owner: str):
+        self.__owner = owner
+        self.__all_devices = []
+
+    def register_device(self, d: Device):
+        self.__all_devices.append(d)
+    
+    def status_report(self):
+        return [device for device in self.__all_devices]
