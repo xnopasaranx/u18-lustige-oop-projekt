cfc0c9c95449ddd364791a1f43e66f5a268cc625
xnopasaranx <t85austria.nerd@gmail.com>
2026-02-27 10:22:05 +0100

    some comments for the rest of the world

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
+# importe der Klassen aus den externen Modulen
+
 from management import SmartHome
-from core import Device
 from beleuchtung import Light
 from klima import Thermostat
 
+# Eine Demo Funktion um einmal eine SmartHome Instanz zu erzeugen und zu demonstrieren 
+# wie die Aggregation und Instanziierung der anderen Klassen funktioniert
+
 def init_demo():
+    # SmartHome Instanz erzeugen
     demo_management = SmartHome(owner="Demo Besitzer")
 
+    # Ein paar Devices mit Subklassen Instanziieren 
     licht1 = Light(device_id="licht1", brightness=0)
     licht2 = Light(device_id="licht2", brightness=0)
 
     therm1 = Thermostat(device_id="therm1", target_temp=20)
     therm2 = Thermostat(device_id="therm2", target_temp=20)
 
+    # Eine Liste mit den Instanzen bauen
     devices = [licht1, licht2, therm1, therm2]
 
+    # In der Schleife hängen wir jetzt jedes Objekt aus der Liste in unser SmartHome Objekt
     for dev in devices:
         demo_management.register_device(dev)
 
+    # Rückgabe der device_id mit dem getter aus der Device Parentklasse, in einer kleinen List Comprehension...
     return [dev.getinfo() for dev in demo_management.status_report()]
 
 print(init_demo())
